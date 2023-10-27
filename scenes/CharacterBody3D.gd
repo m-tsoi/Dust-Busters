de9b590734749dbddbe6dbb36719eeaff8c5f0fe
m-tsoi <mjtsoi@uci.edu>
2023-10-27 16:09:17 -0700

    Updated Test as example of movement

--- scenes/CharacterBody3D.gd
+++ scenes/CharacterBody3D.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 15.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -10,8 +10,8 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity.y -= gravity * delta
+	if not is_on_floor():
+		velocity.y -= gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
