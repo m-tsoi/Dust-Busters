f3463600cd6b08a81485f1a34261361dd88f1823
Khoi Pham <phamkhoiedison@gmail.com>
2023-10-29 11:56:19 -0700

    Make jumping less way less floaty. Make groundspeed faster than airspeed

--- scenes/CharacterBody3D.gd
+++ scenes/CharacterBody3D.gd
@@ -1,17 +1,19 @@
 extends CharacterBody3D
 
 
-const SPEED = 15.0
-const JUMP_VELOCITY = 24
+const SPEED = 15.0 * 1.5
+const JUMP_VELOCITY = 56
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") + 20
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
 	# Add the gravity.
+	var current_speed = SPEED
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		current_speed = current_speed / 1.5
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -22,10 +24,10 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
