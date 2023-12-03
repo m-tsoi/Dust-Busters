cba2b75d22b79b87a59cdc66404104d47619feb1
ringo23333 <92695626+ringo23333@users.noreply.github.com>
2023-12-03 12:52:05 -0800

    Win/lose screen background
    paused level behind win/lose screen

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -41,12 +41,17 @@ func receive_play_level():
 	add_child(current_level)
 	
 func receive_lost():
+	current_level.get_node("InGameUI").queue_free()
+	add_child(lose_scene)
+	move_child(current_level, 0)
+	move_child(lose_scene, 1)
+	get_tree().paused = true
+
+func receive_lose_retry_pressed():
+	get_tree().paused = false
 	remove_child(current_level)
 	current_level.queue_free()
 	reset_autoload()
-	add_child(lose_scene)
-
-func receive_lose_retry_pressed():
 	current_level = level1.instantiate()
 	current_level.connect("lost", receive_lost)
 	current_level.connect("won", receive_won)
@@ -54,20 +59,33 @@ func receive_lose_retry_pressed():
 	add_child(current_level)
 
 func receive_lose_back_pressed():
+	get_tree().paused = false
 	remove_child(lose_scene)
-	add_child(main_menu_scene)
-
-func receive_won():
 	remove_child(current_level)
 	current_level.queue_free()
 	reset_autoload()
+	add_child(main_menu_scene)
+
+func receive_won():
+	current_level.get_node("InGameUI").queue_free()
 	add_child(win_scene)
+	move_child(current_level, 0)
+	move_child(win_scene, 1)
+	get_tree().paused = true
 
 func receive_win_back_pressed():
+	get_tree().paused = false
 	remove_child(win_scene)
+	remove_child(current_level)
+	current_level.queue_free()
+	reset_autoload()
 	add_child(main_menu_scene)
 
 func receive_win_restart_pressed():
+	get_tree().paused = false
+	remove_child(current_level)
+	current_level.queue_free()
+	reset_autoload()
 	current_level = level1.instantiate()
 	current_level.connect("lost", receive_lost)
 	current_level.connect("won", receive_won)
